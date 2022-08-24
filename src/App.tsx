e9a913546b0d822cde8e32005dea498931916998
burlackoff <burlackoff@gmail.com>
2022-08-24 21:26:22 +0300

    test: test add UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Logo} from '@ya.praktikum/react-developer-burger-ui-components' 
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Logo />
       </header>
     </div>
   );
