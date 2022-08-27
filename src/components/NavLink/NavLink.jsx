75479bce3edbd774722c4930aa1ad17f9e08c000
burlackoff <burlackoff@gmail.com>
2022-08-27 20:12:49 +0300

    fixbugs: fix padding for NavLink

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -5,7 +5,7 @@ function NavLink({text, icon, type}) {
   const color = type === 'secondary' ? 'text text_type_main-default text_color_inactive' : 'text text_type_main-default';
 
   return (
-    <a className="Nav-link p-4" href="/#" >
+    <a className="Nav-link pt-4 pb-4 pr-5 pl-5" href="/#" >
       {icon}
       <p className={color}>{text}</p>
     </a>
