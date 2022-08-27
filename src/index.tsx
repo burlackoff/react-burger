1f97ca179aaa0ad5cc1e03d19fda1c293bc84e6e
burlackoff <burlackoff@gmail.com>
2022-08-27 13:42:08 +0300

    feat: add Logo component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppHeader from './components/AppHeader/AppHeader'; 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppHeader />
   </React.StrictMode>
 );
 
