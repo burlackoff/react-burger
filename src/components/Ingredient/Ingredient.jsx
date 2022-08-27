1e36d208f9782da2ef9f07cd7ad750b3b46e2571
burlackoff <burlackoff@gmail.com>
2022-08-27 20:30:24 +0300

    feat: add content Ingredient component

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -1,9 +1,15 @@
-import './Ingredient.css'
+import './Ingredient.css';
+import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 function Ingredient({data}) {
   return (
-    <li className='Ingredient'>
+    <li className='Ingredient pr-4 pl-4'>
       <img src={data.image} alt={data.name} className="Ingredient-image"/>
+      <div className='Ingredient-price mt-1'>
+        <p className='text text_type_digits-default'>{data.price}</p>
+        <CurrencyIcon type="primary" />
+      </div>
+      <p className='text text_type_main-default mt-1'>{data.name}</p>
     </li>
   )
 }
