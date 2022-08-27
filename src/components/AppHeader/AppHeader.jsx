807fd3eced3af4a7ef1ad1b9f309f46f6fb7e68a
burlackoff <burlackoff@gmail.com>
2022-08-27 15:34:30 +0300

    fixbugs: fix style header content

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -8,11 +8,15 @@ import {ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 function AppHeader() {
   return (
-      <header className="App-header p-4 m-4">
-          <NavLink text={"Конструктор"} className="p-4 m-4" icon={<BurgerIcon type="primary" />} />
-          <NavLink text={"Лента заказов"} className="p-4 m-4" icon={<ListIcon type="secondary" />} type="secondary" />
+      <header className="App-header">
+        <div className="App-content m-4">
+          <nav className="App-navbar">
+            <NavLink text={"Конструктор"} className="p-4 m-4" icon={<BurgerIcon type="primary" />} />
+            <NavLink text={"Лента заказов"} className="p-4 m-4" icon={<ListIcon type="secondary" />} type="secondary" />
+          </nav>
           <Logo/>
           <NavLink text={"Личный кабинет"} className="p-4 m-4" icon={<ProfileIcon type="secondary" />} type="secondary" />
+        </div>
       </header>
   );
 }
