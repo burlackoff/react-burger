ebf145861879ecad8f3a31a7cbb91d4fcb475ee8
burlackoff <burlackoff@gmail.com>
2022-08-27 19:49:36 +0300

    feat: new components Ingredient and add favicon

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './BurgerIngredients.css'
 import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
+import Ingredient from '../Ingredient/Ingredient';
 
 function BurgerIngredients({data}) {
-  
   const [current, setCurrent] = React.useState('Булки')
 
   return (
@@ -29,9 +29,14 @@ function BurgerIngredients({data}) {
       </nav>
       <section className='Ingredients-section mt-10'>
         <h2 className='text text_type_main-medium'>Булки</h2>
-        <div>
-          {/* <Ingredient /> */}
-        </div>
+        <ul className='Ingredients-list'>
+          {data
+            .filter((ing) => ing.type === 'bun')
+            .map((ing) => (
+              <Ingredient data={ing} key={ing._id} />
+            ))
+          }
+        </ul>
       </section>
       <section className='Ingredients-section mt-10'>
         <h2 className='text text_type_main-medium'>Соусы</h2>
---
+++ src/components/Ingredient/Ingredient.jsx
@@ -0,0 +1,9 @@
+
+
+function Ingredient({data}) {
+  return (
+    <img src={data.image} alt={data.name}/>
+  )
+}
+
+export default Ingredient
