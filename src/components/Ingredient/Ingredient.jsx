1c467b50ae9fb5805801ecf964226b9e834d527f
burlackoff <burlackoff@gmail.com>
2022-08-28 19:33:10 +0300

    refactor: add module css for Ingredient

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -1,20 +1,19 @@
-import './Ingredient.css';
-import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
-import {Counter} from '@ya.praktikum/react-developer-burger-ui-components';
+import style from './Ingredient.module.css';
+import {CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 
 function Ingredient({data}) {
   const [count, setCount] = React.useState(0);
 
   return (
-    <li className='Ingredient'>
-      <img src={data.image} alt={data.name} className="Ingredient-image pr-4 pl-4"/>
-      <div className='Ingredient-price mt-1 pr-4 pl-4'>
+    <li className={style.ingredient}>
+      <img src={data.image} alt={data.name} className={style.image + ' pr-4 pl-4'}/>
+      <div className={style.price + ' mt-1 pr-4 pl-4'}>
         <p className='text text_type_digits-default'>{data.price}</p>
         <CurrencyIcon type="primary" />
       </div>
       <p className='text text_type_main-default mt-1'>{data.name}</p>
-      <Counter count={count} size="default" onClick={setCount} className='Ingredient-count' />
+      <Counter count={count} size="default" onClick={setCount} className={style.count} />
     </li>
   )
 }
