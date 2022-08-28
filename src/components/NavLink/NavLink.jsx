eee5cebdee065fd16181080b23b5b8a4c49cc474
burlackoff <burlackoff@gmail.com>
2022-08-28 19:34:24 +0300

    refactor: add module css for NavLink

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,11 +1,11 @@
-import './NavLink.css';
+import style from './NavLink.module.css';
 
 
 function NavLink({text, icon, type}) {
   const color = type === 'secondary' ? 'text text_type_main-default text_color_inactive' : 'text text_type_main-default';
 
   return (
-    <a className="Nav-link pt-4 pb-4 pr-5 pl-5" href="/#" >
+    <a className={style.link + ' pt-4 pb-4 pr-5 pl-5'} href="/#" >
       {icon}
       <p className={color}>{text}</p>
     </a>
