106bb5d05981b08741a91c3e0cdc5076e751cb3f
burlackoff <burlackoff@gmail.com>
2022-08-28 18:49:07 +0300

    fixbugs: fix styles list ingredients

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,10 +1,10 @@
 import './BurgerConstructor.css';
-import {ConstructorElement, DragIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import {ConstructorElement, DragIcon, Button, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 function BurgerConstructor({data}) {
   return (
-    <ul className='burger-constructor'>
-      <li>
+    <article className='burger-constructor pl-4'>
+      <div className='burger-constructor-item-bun pr-4'>
         <ConstructorElement
           type="top"
           isLocked={true}
@@ -12,11 +12,11 @@ function BurgerConstructor({data}) {
           price={data[0].price}
           thumbnail={data[0].image}
         />
-      </li>
-      <li>
+      </div>
+      <div>
         <ul className='burger-constructor-filling'>
           {data.filter((ing) => ing.type !== 'bun').map((ing) => (
-            <li key={ing._id}>
+            <li key={ing._id} className='burger-constructor-item pr-2'>
               <DragIcon type="primary" />
               <ConstructorElement
                 text={ing.name}
@@ -26,17 +26,26 @@ function BurgerConstructor({data}) {
             </li>
           ))}
         </ul>
-      </li>
-      <li>
-      <ConstructorElement
+      </div>
+      <div className='burger-constructor-item-bun pr-4'>
+        <ConstructorElement
           type="bottom"
           isLocked={true}
           text={data[0].name + ' (низ)'}
           price={data[0].price}
           thumbnail={data[0].image}
         />
-      </li>
-    </ul>
+      </div>
+      <div className='burger-constructor-order mt-10'>
+        <div className='burger-constructor-price'>
+          <p className="text text_type_digits-medium">610</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </article>
   )
 }
 
