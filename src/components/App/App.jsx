8e878eda9e2988ec029f8892a0b6439546b8786c
burlackoff <burlackoff@gmail.com>
2022-09-05 19:07:44 +0300

    feat: add fetch data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import AppHeader from '../AppHeader/AppHeader'; 
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
+import {data} from '../../utils/data'
 
 function App() {
-  const [state, setState] = React.useState([]);
+  const [state, setState] = React.useState({
+    success: true,
+    data: data
+  });
 
   React.useEffect(() => {
     const getData = async () => {
       const res = await fetch('https://norma.nomoreparties.space/api/ingredients');
       const data = await res.json();
       console.log(data);
-      setState({state: data})
+      setState({success: data.success, data: data.data})
     }
     getData()
   }, [])
@@ -21,8 +25,8 @@ function App() {
       <AppHeader />
       <main className="content-wrapper mb-10">
         
-        <BurgerIngredients data={state}/>
-        <BurgerConstructor data={state}/>
+        <BurgerIngredients data={state.data}/>
+        <BurgerConstructor data={state.data}/>
         
       </main>
     </>
