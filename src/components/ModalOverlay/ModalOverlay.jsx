c703987f27984207c9b1a643a00de7208a1cb060
burlackoff <burlackoff@gmail.com>
2022-09-05 21:18:56 +0300

    feat: add ModalOverlay in DOM

--- src/components/ModalOverlay/ModalOverlay.jsx
+++ src/components/ModalOverlay/ModalOverlay.jsx
@@ -1,5 +1,38 @@
-function ModalOverlay() {
- 
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import style from './ModalOverlay.module.css'
+
+const modal = document.getElementById("modal");
+
+class ModalOverlay extends Component {
+  static _body = document.querySelector('body')
+  static _escape = 'Escape'
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props)
+  }
+
+  openModal() {
+    ModalOverlay._body.style.overflow = 'hidden';
+
+  }
+
+  closeModal() {
+    
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      (
+        <>
+            <div className={style.modal}>
+            </div>
+        </>
+      ), 
+      modal
+    );
+  }
 }
 
 export default ModalOverlay
