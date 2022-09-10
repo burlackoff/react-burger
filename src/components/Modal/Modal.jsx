4756f5c747c477c985e366e1156ae30994661947
burlackoff <burlackoff@gmail.com>
2022-09-10 14:09:31 +0300

    feat: add Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,26 @@
 import ReactDOM from 'react-dom';
 import ModalOverlay from '../ModalOverlay/ModalOverlay';
+import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import style from './Modal.module.css';
+
 
 const modalContainer = document.getElementById("modal");
 
-function Modal({visible, onClose, data}) {
+function Modal({visible, onClose, children, title=true}) {
 
   return ReactDOM.createPortal(
     (
       <ModalOverlay visible={visible} onClose={onClose}>
-        <div className={style.content}>
-          {console.log(data)}
+        <div className={style.modal}>
+          <div className={style.titleBox}>
+            <h2 className='text text_type_main-large'>
+              {title ? 'Детали ингредиента' : ''}
+            </h2>
+            <CloseIcon type="primary" onClick={onClose}/>
+          </div>
+          <div>
+            {children}
+          </div>
         </div>
       </ModalOverlay>
     ),
