13881acb0ffd6524c9a6a107758a58429797bdb9
burlackoff <burlackoff@gmail.com>
2022-09-27 19:53:59 +0300

    feat: add details reducer

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import {GET_INGREDIENTS, GET_BURGER_INGREDIENTS} from '../actions/index';
+import {GET_INGREDIENTS, GET_BURGER_INGREDIENTS, ADD_INGREDIENT_DETAILS, DELETE_INGREDIENT_DETAILS} from '../actions/index';
 
 const initialState = {
   success: false,
@@ -27,16 +27,38 @@ const getBurger = (state = [], action) => {
         ...state,
         action.data
       ]
-      default: 
+    default: 
       return [
         ...state
       ]
   }
 }
 
+const initialStateDetails = {
+  ingredient: {}
+}
 
+const getIngredientDetail = (state = initialStateDetails, action) => {
+  switch (action.type) {
+    case ADD_INGREDIENT_DETAILS:
+      return {
+        ...state,
+        ingredient: action.ingredient
+      }
+    case DELETE_INGREDIENT_DETAILS:
+      return {
+        ...state,
+        ingredient: {}
+      }
+    default:
+      return {
+        ...state
+      }
+  }
+}
 
 export const rootReducer = combineReducers({
   ingredients: getIngredietnsApi,
   burgerIngredients: getBurger,
+  ingredientDetail: getIngredientDetail,
 })
