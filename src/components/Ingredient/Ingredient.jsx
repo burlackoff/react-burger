83c655c448f5339f7907f89a1c7888cd649b86ce
burlackoff <burlackoff@gmail.com>
2022-10-03 20:02:16 +0300

    feat: add useDrag

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -4,15 +4,20 @@ import React from 'react';
 import {ingredientType} from '../../utils/types';
 import {useDispatch} from 'react-redux';
 import {ADD_INGREDIENT_DETAILS} from '../../services/actions/showIngredientDetails';
+import { useDrag } from 'react-dnd';
 
 function Ingredient({data}) {
   const [count, setCount] = React.useState(0);
   const dispatch = useDispatch();
+  const [, dragRef] = useDrag({
+    type: 'ingredient',
+    item: {},
+  });
 
   return (
     <>
-      <li className={style.ingredient} onClick={() => dispatch({type: ADD_INGREDIENT_DETAILS, ingredient: data})}>
-        <img src={data.image} alt={data.name} className={`${style.image} pr-4 pl-4`}/>
+      <li className={style.ingredient} draggable ref={dragRef} onClick={() => dispatch({type: ADD_INGREDIENT_DETAILS, ingredient: data})}>
+        <img src={data.image} alt={data.name} className={`${style.image} pr-4 pl-4`} />
         <div className={`${style.price} mt-1 pr-4 pl-4`}>
           <p className='text text_type_digits-default'>{data.price}</p>
           <CurrencyIcon type="primary" />
