1a308b8073ad75a1d38354bdfae68fb3c4fe8434
burlackoff <burlackoff@gmail.com>
2022-10-10 21:36:27 +0300

    refactor: ref init store

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './components/App/App';
-import { rootReducer } from './services/reducers';
-import { legacy_createStore as createStore, applyMiddleware} from 'redux';
+import { store } from './services/store';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/services/store.js
@@ -0,0 +1,6 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers';
+import { legacy_createStore as createStore, applyMiddleware} from 'redux';
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
